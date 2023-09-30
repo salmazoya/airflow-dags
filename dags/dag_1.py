23c1e9c6fb3e30b97763b1c0f980ccf71ab6acb9
deacademy <144675627+deacademy@users.noreply.github.com>
2023-09-30 13:22:01 +0530

    Update dag_1.py

--- dags/dag_1.py
+++ dags/dag_1.py
@@ -3,7 +3,14 @@ from datetime import datetime
 from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
+import boto3
+from botocore.exceptions import ClientError
+import json
+import logging
 
+s3_client = boto3.client('s3')
+
+logger = logging.getLogger(__name__)
 
 with DAG(dag_id='demo', start_date=datetime(2023,9,25), schedule='0 0 * * *') as dag:
 
@@ -11,6 +18,15 @@ with DAG(dag_id='demo', start_date=datetime(2023,9,25), schedule='0 0 * * *') as
 
     @task()
     def airflow():
-        print("airflow")
+        try:
+            print("airflow")
+            json_object = 'Hello from airflow to s3'
+            s3_client.put_object(
+                Body=json.dumps(json_object),
+                Bucket='airflow-user-posts-data',
+                Key='dummy_data/airflow_msg.json'
+            )
+        except ClientError as e:
+            logging.error(e)
     
     hello >> airflow()
